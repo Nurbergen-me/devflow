a1aa4cfc8e018e242b474dce5c69554f52664cc8
nurik.eleshov <53029070+Nurbergen-me@users.noreply.github.com>
2026-03-03 07:32:00 +0500

    Implement Action Handler

---
+++ lib/handlers/action.ts
@@ -0,0 +1,42 @@
+"use server";
+
+import { ZodSchema } from "zod/v3";
+import { z, ZodError } from "zod";
+import { UnauthorizedError, ValidationError } from "@/lib/http-errors";
+import { Session } from "next-auth";
+import { auth } from "@/auth";
+import dbConnect from "@/lib/mongoose";
+
+type ActionOptions<T> = {
+  params?: T;
+  schema?: ZodSchema<T>;
+  authorize?: boolean;
+};
+async function action<T>({ params, schema, authorize }: ActionOptions<T>) {
+  if (schema && params) {
+    try {
+      schema.parse(params);
+    } catch (error) {
+      if (error instanceof ZodError) {
+        return new ValidationError(z.flattenError(error).fieldErrors as Record<string, string[]>);
+      } else {
+        return new Error("Schema validation error");
+      }
+    }
+  }
+
+  let session: Session | null = null;
+  if (authorize) {
+    session = await auth();
+
+    if (!session) {
+      return new UnauthorizedError();
+    }
+  }
+
+  await dbConnect();
+
+  return { params, session };
+}
+
+export default action;
