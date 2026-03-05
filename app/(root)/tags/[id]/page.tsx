2d4989c034bcc8083fdef42316bf8b9c2fe5203e
nurik.eleshov <53029070+Nurbergen-me@users.noreply.github.com>
2026-03-05 13:49:23 +0500

    Implement Tag Question Page

---
+++ app/(root)/tags/[id]/page.tsx
@@ -0,0 +1,59 @@
+import React from "react";
+import { RouteParams } from "@/types/global";
+import { getTagQuestions } from "@/lib/actions/tag.action";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import ROUTES from "@/constants/routes";
+import LocalSearch from "@/components/search/LocalSearch";
+import HomeFilters from "@/components/filters/HomeFilters";
+import DataRenderer from "@/components/DataRenderer";
+import { EMPTY_QUESTION } from "@/constants/states";
+import QuestionCard from "@/components/cards/QuestionCard";
+
+const Page = async ({ params, searchParams }: RouteParams) => {
+  const { id } = await params;
+  const { page, pageSize, query } = await searchParams;
+
+  const { success, data, error } = await getTagQuestions({
+    tagId: id,
+    page: Number(page) || 1,
+    pageSize: Number(pageSize) || 10,
+    query: query || "",
+  });
+  const questions = data?.questions;
+
+  return (
+    <>
+      <section className="flex w-full flex-col-reverse justify-between gap-4 sm:flex-row sm:items-center">
+        <h1 className="h1-bold">{data?.tag.name}</h1>
+      </section>
+
+      <section className="mt-11">
+        <LocalSearch
+          imgSrc="/icons/search.svg"
+          placeholder="Search questions..."
+          otherClasses="flex-1"
+          route={ROUTES.TAG(id)}
+        />
+      </section>
+
+      <DataRenderer
+        success={success}
+        error={error}
+        data={questions}
+        empty={EMPTY_QUESTION}
+        render={(questions) => (
+          <div className="mt-10 flex w-full flex-col gap-6">
+            {questions.map((question) => (
+              <QuestionCard
+                key={question._id}
+                question={question}
+              />
+            ))}
+          </div>
+        )}
+      />
+    </>
+  );
+};
+export default Page;
